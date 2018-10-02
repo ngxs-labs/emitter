a3f0b248d27559dc0581959cd0592a1814a3eaec
Artur <arthurandrosovich@gmail.com>
2018-10-02 16:38:22 +0300

    feat(emitter): rewrite tests for `@PayloadEmitter`

--- projects/emitter/tests/emitter.spec.ts
+++ projects/emitter/tests/emitter.spec.ts
@@ -40,174 +40,6 @@ describe('Emitter', () => {
         expect(BarFooMeta.type).toBe('BarState.foo');
     });
 
-    it('should add todo using @Emitter() decorator', () => {
-        @State<Todo[]>({
-            name: 'todos',
-            defaults: []
-        })
-        class TodosState {
-            @Emitter()
-            public static addTodo(ctx: StateContext<Todo[]>, action: EmitterAction<Todo>) {
-                ctx.setState([...ctx.getState(), action.payload!]);
-            }
-        }
-
-        TestBed.configureTestingModule({
-            imports: [
-                NgxsModule.forRoot([TodosState]),
-                NgxsEmitPluginModule.forRoot()
-            ]
-        });
-
-        const store: EmitStore = TestBed.get(EmitStore);
-
-        store.emitter<Todo>(TodosState.addTodo).emit({
-            text: 'buy coffee',
-            completed: false
-        });
-
-        const todoLength = store.selectSnapshot<Todo[]>(state => state.todos).length;
-        expect(todoLength).toBe(1);
-    });
-
-    it('should dispatch an action from the sub state', () => {
-        @State({
-            name: 'bar2',
-            defaults: 10
-        })
-        class Bar2State {
-            @Emitter()
-            public static foo2({ setState }: StateContext<number>) {
-                setState(20);
-            }
-        }
-
-        @State({
-            name: 'bar',
-            defaults: {},
-            children: [Bar2State]
-        })
-        class BarState {}
-
-        TestBed.configureTestingModule({
-            imports: [
-                NgxsModule.forRoot([BarState, Bar2State]),
-                NgxsEmitPluginModule.forRoot()
-            ]
-        });
-
-        const store: EmitStore = TestBed.get(EmitStore);
-        store.emitter(Bar2State.foo2).emit();
-
-        const bar2Value = store.selectSnapshot(state => state.bar).bar2;
-        expect(bar2Value).toBe(20);
-    });
-
-    it('should throw an error that such `type` already exists', () => {
-        try {
-            @State({
-                name: 'bar',
-                defaults: 10
-            })
-            class BarState {
-                @Emitter({ type: 'foo' })
-                public static foo1() {}
-
-                @Emitter({ type: 'foo' })
-                public static foo2() {}
-            }
-
-            TestBed.configureTestingModule({
-                imports: [
-                    NgxsModule.forRoot([BarState]),
-                    NgxsEmitPluginModule.forRoot()
-                ]
-            });
-        } catch ({ message }) {
-            expect(message).toBe('Method decorated with such type `foo` already exists');
-        }
-    });
-
-    it('should dispatch an action using @Emitter() after delay', () => {
-        @Injectable()
-        class ApiService {
-            private size = 10;
-
-            public getTodosFromServer(length: number): Observable<Todo[]> {
-                return of(this.generateTodoMock(length)).pipe(delay(1000));
-            }
-
-            private generateTodoMock(size?: number): Todo[] {
-                const length = size || this.size;
-                return Array.from({ length }).map(() => ({
-                    text: 'buy some coffee',
-                    completed: false
-                }));
-            }
-        }
-
-        @State<Todo[]>({
-            name: 'todos',
-            defaults: []
-        })
-        class TodosState {
-            public static injector: Injector | null = null;
-
-            constructor(injector: Injector) {
-                TodosState.injector = injector;
-            }
-
-            @Emitter({ type: '@@[Todos] Set todos sync' })
-            public static async setTodosSync({ setState }: StateContext<Todo[]>) {
-                setState(
-                    await TodosState.injector!
-                        .get<ApiService>(ApiService)
-                        .getTodosFromServer(10)
-                        .toPromise()
-                );
-            }
-
-            @Emitter({ type: '@@[Todos] Set todos' })
-            public static setTodos({ setState }: StateContext<Todo[]>) {
-                return TodosState.injector!
-                    .get<ApiService>(ApiService)
-                    .getTodosFromServer(5)
-                    .pipe(
-                        take(1),
-                        tap(todos => setState(todos))
-                    );
-            }
-        }
-
-        TestBed.configureTestingModule({
-            imports: [
-                NgxsModule.forRoot([TodosState]),
-                NgxsEmitPluginModule.forRoot()
-            ],
-            providers: [
-                ApiService
-            ]
-        });
-
-        const store: EmitStore = TestBed.get(EmitStore);
-
-        store
-            .emitter<void>(TodosState.setTodosSync)
-            .emit()
-            .subscribe(() => {
-                const todoLength = store.selectSnapshot<Todo[]>(state => state.todos).length;
-                expect(todoLength).toBe(10);
-            });
-
-        store
-            .emitter<void>(TodosState.setTodos)
-            .emit()
-            .subscribe(() => {
-                const todoLength = store.selectSnapshot<Todo[]>(state => state.todos).length;
-                expect(todoLength).toBe(5);
-            });
-    });
-
     it('should decorate property with @PayloadEmitter() decorator', () => {
         @State<Todo[]>({
             name: 'todos',
@@ -267,7 +99,7 @@ describe('Emitter', () => {
             ]
         });
 
-        const store: EmitStore = TestBed.get(EmitStore);
+        const store: Store = TestBed.get(Store);
         const fixture = TestBed.createComponent(MockComponent);
 
         fixture.componentInstance.addTodoAction!.emit({
@@ -277,5 +109,205 @@ describe('Emitter', () => {
 
         const todoLength = store.selectSnapshot<Todo[]>(state => state.todos).length;
         expect(todoLength).toBe(1);
+    });
+
+    it('should add todo using @Emitter() decorator', () => {
+        @State<Todo[]>({
+            name: 'todos',
+            defaults: []
+        })
+        class TodosState {
+            @Emitter()
+            public static addTodo(ctx: StateContext<Todo[]>, action: EmitterAction<Todo>) {
+                ctx.setState([...ctx.getState(), action.payload!]);
+            }
+        }
+
+        @Component({
+            template: ''
+        })
+        class MockComponent {
+            @PayloadEmitter(TodosState.addTodo)
+            public addTodo: Emittable<Todo> | undefined;
+        }
+
+        TestBed.configureTestingModule({
+            imports: [
+                NgxsModule.forRoot([TodosState]),
+                NgxsEmitPluginModule.forRoot()
+            ],
+            declarations: [
+                MockComponent
+            ]
+        });
+
+        const store: Store = TestBed.get(Store);
+        const fixture = TestBed.createComponent(MockComponent);
+
+        fixture.componentInstance.addTodo!.emit({
+            text: 'buy coffee',
+            completed: false
+        });
+
+        const todoLength = store.selectSnapshot<Todo[]>(state => state.todos).length;
+        expect(todoLength).toBe(1);
+    });
+
+    it('should dispatch an action from the sub state', () => {
+        @State({
+            name: 'bar2',
+            defaults: 10
+        })
+        class Bar2State {
+            @Emitter()
+            public static foo2({ setState }: StateContext<number>) {
+                setState(20);
+            }
+        }
+
+        @State({
+            name: 'bar',
+            defaults: {},
+            children: [Bar2State]
+        })
+        class BarState {}
+
+        @Component({
+            template: ''
+        })
+        class MockComponent {
+            @PayloadEmitter(Bar2State.foo2)
+            public foo2: Emittable<void> | undefined;
+        }
+
+        TestBed.configureTestingModule({
+            imports: [
+                NgxsModule.forRoot([BarState, Bar2State]),
+                NgxsEmitPluginModule.forRoot()
+            ],
+            declarations: [
+                MockComponent
+            ]
+        });
+
+        const store: Store = TestBed.get(Store);
+        const fixture = TestBed.createComponent(MockComponent);
+
+        fixture.componentInstance.foo2!.emit();
+
+        const bar2Value = store.selectSnapshot(state => state.bar).bar2;
+        expect(bar2Value).toBe(20);
+    });
+
+    it('should throw an error that such `type` already exists', () => {
+        try {
+            @State({
+                name: 'bar',
+                defaults: 10
+            })
+            class BarState {
+                @Emitter({ type: 'foo' })
+                public static foo1() {}
+
+                @Emitter({ type: 'foo' })
+                public static foo2() {}
+            }
+
+            TestBed.configureTestingModule({
+                imports: [
+                    NgxsModule.forRoot([BarState]),
+                    NgxsEmitPluginModule.forRoot()
+                ]
+            });
+        } catch ({ message }) {
+            expect(message).toBe('Method decorated with such type `foo` already exists');
+        }
+    });
+
+    it('should dispatch an action using @Emitter() after delay', () => {
+        @Injectable()
+        class ApiService {
+            private size = 10;
+
+            public getTodosFromServer(length: number): Observable<Todo[]> {
+                return of(this.generateTodoMock(length)).pipe(delay(1000));
+            }
+
+            private generateTodoMock(size?: number): Todo[] {
+                const length = size || this.size;
+                return Array.from({ length }).map(() => ({
+                    text: 'buy some coffee',
+                    completed: false
+                }));
+            }
+        }
+
+        @State<Todo[]>({
+            name: 'todos',
+            defaults: []
+        })
+        class TodosState {
+            public static api: ApiService | null = null;
+
+            constructor(injector: Injector) {
+                TodosState.api = injector.get<ApiService>(ApiService);
+            }
+
+            @Emitter({ type: '@@[Todos] Set todos sync' })
+            public static async setTodosSync({ setState }: StateContext<Todo[]>) {
+                setState(
+                    await TodosState.api!
+                        .getTodosFromServer(10)
+                        .toPromise()
+                );
+            }
+
+            @Emitter({ type: '@@[Todos] Set todos' })
+            public static setTodos({ setState }: StateContext<Todo[]>) {
+                return TodosState.api!
+                    .getTodosFromServer(5)
+                    .pipe(
+                        take(1),
+                        tap(todos => setState(todos))
+                    );
+            }
+        }
+
+        @Component({
+            template: ''
+        })
+        class MockComponent {
+            @PayloadEmitter(TodosState.setTodosSync)
+            public setTodosSync: Emittable<Todo[]> | undefined;
+
+            @PayloadEmitter(TodosState.setTodos)
+            public setTodos: Emittable<Todo[]> | undefined;
+        }
+
+        TestBed.configureTestingModule({
+            imports: [
+                NgxsModule.forRoot([TodosState]),
+                NgxsEmitPluginModule.forRoot()
+            ],
+            declarations: [
+                MockComponent
+            ],
+            providers: [
+                ApiService
+            ]
+        });
+
+        const store: Store = TestBed.get(Store);
+        const fixture = TestBed.createComponent(MockComponent);
+
+        fixture.componentInstance.setTodosSync!.emit().subscribe(() => {
+            const todoLength = store.selectSnapshot<Todo[]>(state => state.todos).length;
+            expect(todoLength).toBe(10);
+        });
+            
+        fixture.componentInstance.setTodos!.emit().subscribe(() => {
+            const todoLength = store.selectSnapshot<Todo[]>(state => state.todos).length;
+            expect(todoLength).toBe(5);
+        });
     });
 });
