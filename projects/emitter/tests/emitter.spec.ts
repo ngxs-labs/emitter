9f0ad2e99c7cc708ab4ee0954eec5c0a33e38cd3
Artur <arthurandrosovich@gmail.com>
2018-10-02 16:39:37 +0300

    feat(emitter): rewrite tests for `@PayloadEmitter`

--- projects/emitter/tests/emitter.spec.ts
+++ projects/emitter/tests/emitter.spec.ts
@@ -8,7 +8,6 @@ import { delay, take, tap } from 'rxjs/operators';
 import { Emitter } from '../src/lib/core/decorators/emitter';
 import { EMITTER_META_KEY, Emittable } from '../src/lib/core/internal/internals';
 import { EmitterAction } from '../src/lib/core/actions/actions';
-import { EmitStore } from '../src/lib/emit.service';
 import { PayloadEmitter } from '../src/lib/core/decorators/payload-emitter';
 import { NgxsEmitPluginModule } from '../src/lib/emit.module';
 
