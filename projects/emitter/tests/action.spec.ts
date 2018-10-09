2636cc8f7da499f4641a86b888475592efa557ff
Artur <arthurandrosovich@gmail.com>
2018-10-09 13:58:11 +0300

    feat(emitter): ! add tests

--- projects/emitter/tests/action.spec.ts
+++ projects/emitter/tests/action.spec.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { Actions, NgxsModule, State, StateContext } from '@ngxs/store';
+import { Actions, NgxsModule, State, StateContext, ofActionDispatched } from '@ngxs/store';
 
 import { throwError } from 'rxjs';
 
@@ -11,6 +11,84 @@ import { NgxsEmitPluginModule } from '../src/lib/emit.module';
 import { ofEmittableDispatched, ofEmittableErrored } from '../src/lib/core/operators/of-emittable';
 
 describe('Actions', () => {
+    it('should intercept custom action that is defined in the @Emitter() decorator', () => {
+        class Increment {
+            public static type = '[Counter] Increment';
+        }
+
+        class Decrement {
+            public static type = '[Counter] Decrement';
+        }
+
+        class MultiplyBy2 {
+            public static type = '[Counter] Multiply by 2';
+        }
+
+        @State<number>({
+            name: 'counter',
+            defaults: 0
+        })
+        class CounterState {
+            @Emitter({
+                action: Increment
+            })
+            public static increment({ setState, getState }: StateContext<number>) {
+                setState(getState() + 1);
+            }
+
+            @Emitter({
+                action: Decrement
+            })
+            public static decrement({ setState, getState }: StateContext<number>) {
+                setState(getState() - 1);
+            }
+
+            @Emitter({
+                action: MultiplyBy2
+            })
+            public static multiplyBy2({ setState, getState }: StateContext<number>) {
+                setState(getState() * 2);
+            }
+        }
+
+        @Component({
+            template: ''
+        })
+        class MockComponent {
+            @PayloadEmitter(CounterState.increment)
+            public increment: Emittable<void> | undefined;
+
+            @PayloadEmitter(CounterState.decrement)
+            public decrement: Emittable<void> | undefined;
+
+            @PayloadEmitter(CounterState.multiplyBy2)
+            public multiplyBy2: Emittable<void> | undefined;
+        }
+
+        TestBed.configureTestingModule({
+            imports: [
+                NgxsModule.forRoot([CounterState]),
+                NgxsEmitPluginModule.forRoot()
+            ],
+            declarations: [
+                MockComponent
+            ]
+        });
+
+        const fixture = TestBed.createComponent(MockComponent);
+        const actions$: Actions = TestBed.get(Actions);
+
+        actions$.pipe(
+            ofActionDispatched(Increment)
+        ).subscribe((action) => {
+            expect(action.constructor).toBe(Increment);
+        });
+
+        fixture.componentInstance.decrement!.emit();
+        fixture.componentInstance.increment!.emit();
+        fixture.componentInstance.multiplyBy2!.emit();
+    });
+
     it('should intercept only CounterState.increment emitter', () => {
         @State<number>({
             name: 'counter',
