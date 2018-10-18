d6823af7816051a3df84b3e12b796042c15719c1
Artur <arthurandrosovich@gmail.com>
2018-10-18 17:17:29 +0300

    chore(emitter): + add custom chrome launcher

--- projects/emitter/karma.conf.js
+++ projects/emitter/karma.conf.js
@@ -1,7 +1,9 @@
 module.exports = (config) => {
-    config.set({
+    const configuration = {
         basePath: '',
+
         frameworks: ['jasmine', '@angular-devkit/build-angular'],
+
         plugins: [
             require('karma-jasmine'),
             require('karma-chrome-launcher'),
@@ -9,20 +11,42 @@ module.exports = (config) => {
             require('karma-coverage-istanbul-reporter'),
             require('@angular-devkit/build-angular/plugins/karma')
         ],
+
         client: {
             clearContext: false
         },
+
         coverageIstanbulReporter: {
             dir: require('path').join(__dirname, '../../coverage'),
             reports: ['html', 'lcovonly'],
             fixWebpackSourcePaths: true
         },
+
         reporters: ['progress', 'kjhtml'],
+
         port: 9876,
+
         colors: true,
+
         logLevel: config.LOG_INFO,
+
         autoWatch: true,
+
         browsers: ['Chrome'],
-        singleRun: false
-    });
+
+        singleRun: false,
+
+        customLaunchers: {
+            Chrome_travis_ci: {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
+        }
+    };
+
+    if (process.env.TRAVIS) {
+        configuration.browsers = ['Chrome_travis_ci'];
+    }
+
+    config.set(configuration);
 };
