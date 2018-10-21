d3feb6612b9785d6ba7495dcefa4ee55eed479b2
Artur Androsovych <arthurandrosovich@gmail.com>
2018-10-21 23:00:09 +0300

    chore(emitter): ! move emitter package from `projects/emitter` to `sr… (#19)
    * chore(emitter): ! move emitter package from `projects/emitter` to `src` folder

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,31 +1,52 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+module.exports = (config) => {
+    const configuration = {
+        basePath: '',
 
-module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
-  });
+        frameworks: ['jasmine', '@angular-devkit/build-angular'],
+
+        plugins: [
+            require('karma-jasmine'),
+            require('karma-chrome-launcher'),
+            require('karma-jasmine-html-reporter'),
+            require('karma-coverage-istanbul-reporter'),
+            require('@angular-devkit/build-angular/plugins/karma')
+        ],
+
+        client: {
+            clearContext: false
+        },
+
+        coverageIstanbulReporter: {
+            dir: require('path').join(__dirname, '../../coverage'),
+            reports: ['html', 'lcovonly'],
+            fixWebpackSourcePaths: true
+        },
+
+        reporters: ['progress', 'kjhtml'],
+
+        port: 9876,
+
+        colors: true,
+
+        logLevel: config.LOG_INFO,
+
+        autoWatch: true,
+
+        browsers: ['Chrome'],
+
+        singleRun: false,
+
+        customLaunchers: {
+            Chrome_travis_ci: {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
+        }
+    };
+
+    if (process.env.TRAVIS) {
+        configuration.browsers = ['Chrome_travis_ci'];
+    }
+
+    config.set(configuration);
 };
