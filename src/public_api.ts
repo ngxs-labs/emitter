2fc464f2b6c71edbe014c0687a4ed581a48efb1e
Artur <arthurandrosovich@gmail.com>
2018-10-22 15:31:52 +0300

    fix(emitter): ! fix public exports

--- src/public_api.ts
+++ src/public_api.ts
@@ -1,8 +1,8 @@
-export * from './lib/core/decorators/receiver';
+export { Receiver } from './lib/core/decorators/receiver';
 export { Emitter } from './lib/core/decorators/emitter';
 export { Emittable, OfEmittableActionContext } from './lib/core/internal/internals';
-export * from './lib/core/actions/actions';
-export * from './lib/emit.module';
+export { EmitterAction } from './lib/core/actions/actions';
+export { NgxsEmitPluginModule } from './lib/emit.module';
 export {
     ofEmittableDispatched,
     ofEmittableSuccessful,
