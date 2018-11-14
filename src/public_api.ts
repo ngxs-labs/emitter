4a7daf1f94744daaac683efcda733026bee54a02
Artur <arthurandrosovich@gmail.com>
2018-11-14 20:25:56 +0200

    feat(store): export `EmitStore` class

--- src/public_api.ts
+++ src/public_api.ts
@@ -3,6 +3,7 @@ export { Emitter } from './lib/core/decorators/emitter';
 export { Emittable, OfEmittableActionContext } from './lib/core/internal/internals';
 export { EmitterAction } from './lib/core/actions/actions';
 export { NgxsEmitPluginModule } from './lib/emit.module';
+export { EmitStore } from './lib/core/services/emit-store.service';
 export {
     ofEmittableDispatched,
     ofEmittableSuccessful,
