fee06b241c425affbc264c0277f4c6628ead6829
Artur <arthurandrosovich@gmail.com>
2018-11-21 21:06:02 +0200

    feat(emitter): refactor `@Receiver` decorator

--- src/lib/core/decorators/receiver.ts
+++ src/lib/core/decorators/receiver.ts
@@ -9,45 +9,41 @@ function generateHash(): string {
 }
 
 /**
- * @internal
  * @param options - Options passed to the `@Receiver()` decorator
- * @param target - Decorated class
- * @param key - Decorated property name
- * @returns - Action type
+ * @param target - Decorated target
+ * @param key - Decorated key
  */
-function getActionType(options: Partial<ReceiverMetaData>, target: Function, key: string): string {
-    const action = options && options.action;
+export function getActionProperties(options: Partial<ReceiverMetaData> | undefined, target: Function, key: string) {
+    const defaultProperties = {
+        type: `[ID:${generateHash()}] ${target.name}.${key}`,
+        payload: undefined,
+        action: undefined,
+        cancelUncompleted: true,
+    };
+
+    if (!options) {
+        return defaultProperties;
+    }
+
+    const { type, payload, action, cancelUncompleted } = options;
+
+    if (type) {
+        defaultProperties.type = type;
+    }
+
+    if (payload) {
+        defaultProperties.payload = payload;
+    }
 
     if (action) {
-        if (typeof action.type !== 'string') {
-            throw new Error(`${action.name}'s type should be defined as a static property \`type\``);
-        }
-
-        return action.type;
+        defaultProperties.type = action.type!;
     }
 
-    return (options && options.type) || `[ID:${generateHash()}] ${target.name}.${key}`;
-}
-
-/**
- * @internal
- * @param options - Options passed to the `@Receiver()` decorator
- */
-function getActionProperties(options: Partial<ReceiverMetaData> | undefined): Partial<ReceiverMetaData> {
-    if (!options) {
-        return {
-            action: undefined,
-            payload: undefined,
-            cancelUncompleted: true
-        };
+    if (typeof cancelUncompleted === 'boolean') {
+        defaultProperties.cancelUncompleted = cancelUncompleted;
     }
 
-    let cancelUncompleted = true;
-    if (typeof options.cancelUncompleted === 'boolean') {
-        cancelUncompleted = options.cancelUncompleted;
-    }
-
-    return { action: options.action, payload: options.payload, cancelUncompleted };
+    return defaultProperties;
 }
 
 /**
@@ -69,15 +65,13 @@ export function Receiver(options?: Partial<ReceiverMetaData>): MethodDecorator {
         }
 
         const meta = ensureStoreMetadata(target);
-        const type = getActionType(options as Partial<ReceiverMetaData>, target, key);
+        const { type, payload, action, cancelUncompleted } = getActionProperties(options, target, key);
 
         if (meta.actions.hasOwnProperty(type)) {
             throw new Error(`Method decorated with such type \`${type}\` already exists`);
         }
 
-        const { action, payload, cancelUncompleted } = getActionProperties(options);
-
-        meta.actions[type] = [{
+        meta.actions[type!] = [{
             fn: `${key}`,
             options: { cancelUncompleted },
             type
